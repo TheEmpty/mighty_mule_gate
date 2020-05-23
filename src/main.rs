ff731dcf68baaf75786d9a6024ffa7cc4066e15a
Myles Empty Best <myles@empty.best>
2020-05-22 23:09:41 -0700

    Setup a hyper server example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+
+async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(Body::from(
+            "Hello world"
+            ))),
+
+        // 404 / catch-all
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            return Ok(not_found);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // TODO: configurable port.
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3005));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
