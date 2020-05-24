a38732d8b60d73aaaa1e0195406c289d6676c058
Myles Empty Best <myles@empty.best>
2020-05-23 20:01:18 -0700

    Some refactoring around the server mapping
    Also removing the threading in the controller to focus on using switches to understand the state instead of guestimating

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,23 @@
 mod gate;
 mod service_configuration;
+mod server;
 
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::str::FromStr;
-use std::thread;
-use std::collections::HashMap;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use http::header::HeaderValue;
+use hyper::Server;
 use gate::Gate;
-
-static mut GATE: Option<Gate> = None;
-
-// TODO: break this up
-async fn router(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let params: HashMap<String, String> = req
-        .uri()
-        .query()
-        .map(|v| {
-            url::form_urlencoded::parse(v.as_bytes())
-                .into_owned()
-                .collect()
-        })
-        .unwrap_or_else(HashMap::new);
-
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => {
-            let body = Body::from("Might Mule Gate API");
-            let mut response = Response::new(body);
-            response.headers_mut().insert("Content-Type", HeaderValue::from_str("text/plain").unwrap());
-            return Ok(response);
-        }
-
-        (&Method::GET, "/gate") => {
-            unsafe {
-                let gate = GATE.as_mut().unwrap();
-                gate.clear_expired_locks();
-                let gate_json = serde_json::to_string(&GATE).unwrap();
-                let body = Body::from(gate_json);
-                let mut response = Response::new(body);
-                response.headers_mut().insert("Content-Type",  HeaderValue::from_str("application/json").unwrap());
-                return Ok(response);
-            }
-        },
-
-        (&Method::POST, "/gate") => {
-            unsafe {
-                let mut operation = false;
-
-                let desired_state_param = params.get("desired_state");
-                if desired_state_param.is_some() {
-                    operation = true;
-                    let desired_state = gate::State::from_str(desired_state_param.unwrap());
-                    if desired_state.is_ok() {
-                        thread::spawn(move || {
-                            GATE.as_mut().unwrap().change_state(desired_state.unwrap());
-                        });
-                    } else {
-                        let body = Body::from(format!("{{\"error\": \"Invalid state, {}\"}}", desired_state_param.unwrap()));
-                        let mut response = Response::new(body);
-                        *response.status_mut() = StatusCode::BAD_REQUEST;
-                        return Ok(response);
-                    }
-                }
-
-                let lock_state_param = params.get("lock_state");
-                if lock_state_param.is_some() {
-                    operation = true;
-                    let lock_state = gate::State::from_str(lock_state_param.unwrap());
-                    if lock_state.is_ok() {
-                        // TODO: TTL is passed in and server_config has a max TTL setting
-                        // Note: a real TTL would be like 15-60 minutes
-                        GATE.as_mut().unwrap().hold_state(lock_state.unwrap(), std::time::Duration::from_secs(180));
-                    } else {
-                        let body = Body::from(format!("{{\"error\": \"Invalid state, {}\"}}", lock_state_param.unwrap()));
-                        let mut response = Response::new(body);
-                        *response.status_mut() = StatusCode::BAD_REQUEST;
-                        return Ok(response);
-                    }
-                }
-
-                if operation == false {
-                    let body = Body::from("{\"error\": \"no operation requested\"}");
-                    let mut response = Response::new(body);
-                    *response.status_mut() = StatusCode::BAD_REQUEST;
-                    return Ok(response);
-                }
-
-                // TODO: don't do this async once we get rid of the Thread::Sleeping
-                let body = Body::from(format!("{{\"success\": {}}}", "maybe"));
-                let mut response = Response::new(body);
-                response.headers_mut().insert("Content-Type",  HeaderValue::from_str("application/json").unwrap());
-                return Ok(response);
-            }
-        }
-
-        // 404 / catch-all
-        _ => {
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            return Ok(not_found);
-        }
-    }
-}
-
 
 #[tokio::main]
 async fn main() {
     let conf = service_configuration::load();
     unsafe {
-        GATE = Some(Gate::new(conf.gate_configuration));
+        server::GATE = Some(Gate::new(conf.gate_configuration));
     }
 
     let addr = SocketAddr::from(([0, 0, 0, 0], conf.server_port));
     let service = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(router))
+        Ok::<_, Infallible>(service_fn(server::handle))
     });
     let server = Server::bind(&addr).serve(service);
 
---
+++ src/server.rs
@@ -0,0 +1,159 @@
+use std::convert::Infallible;
+use std::str::FromStr;
+use std::collections::HashMap;
+use hyper::{Body, Method, Request, Response, StatusCode};
+use http::header::HeaderValue;
+use crate::gate::Gate;
+use crate::gate;
+
+pub static mut GATE: Option<Gate> = None;
+
+fn get_params(req: &Request<Body>) -> HashMap<String, String> {
+    return req
+        .uri()
+        .query()
+        .map(|v| {
+            url::form_urlencoded::parse(v.as_bytes())
+                .into_owned()
+                .collect()
+        })
+        .unwrap_or_else(HashMap::new);
+}
+
+fn easy_json_response(json: String) -> Response<Body> {
+    let body = Body::from(json);
+    let mut response = Response::new(body);
+    set_json_response_type(&mut response);
+    return response;
+}
+
+fn set_json_response_type(response: &mut Response<Body>) -> () {
+    response.headers_mut().insert("Content-Type",  HeaderValue::from_str("application/json").unwrap());
+}
+
+// GET /
+fn get_root() -> Response<Body> {
+    let body = Body::from("Might Mule Gate API");
+    let mut response = Response::new(body);
+    response.headers_mut().insert("Content-Type", HeaderValue::from_str("text/plain").unwrap());
+    return response;
+}
+
+// GET /gate
+fn get_gate() -> Response<Body> {
+    let gate_json: String;
+    unsafe {
+        let gate = GATE.as_mut().unwrap();
+        gate.clear_expired_locks();
+        gate_json = serde_json::to_string(&GATE).unwrap();
+    };
+
+    let body = Body::from(gate_json);
+    let mut response = Response::new(body);
+    set_json_response_type(&mut response);
+    return response;
+}
+
+fn invalid_state_response(desired_state: &String) -> Response<Body> {
+    let json = format!("{{\"error\": \"Invalid state, {}\"}}", desired_state).to_string();
+    let mut response = easy_json_response(json);
+    *response.status_mut() = StatusCode::BAD_REQUEST;
+    return response;
+}
+
+fn set_desired_state(req: &Request<Body>) -> Option<Response<Body>> {
+    let params = get_params(req);
+    let desired_state_param = params.get("desired_state").unwrap();
+    let desired_state = gate::State::from_str(desired_state_param);
+
+    if desired_state.is_ok() {
+        let state_changed: bool;
+        unsafe {
+            state_changed = GATE.as_mut().unwrap().change_state(desired_state.unwrap());
+        }
+        if state_changed == false {
+            let json_response = "{\"error\": \"Could not move to desired_state. Most likely due to a lock.\"}".to_string();
+            return Some(easy_json_response(json_response));
+        }
+        return None;
+    } else {
+        return Some(invalid_state_response(desired_state_param));
+    }
+}
+
+fn lock_state(req: &Request<Body>) -> Option<Response<Body>> {
+    let params = get_params(req);
+    let lock_state_param = params.get("lock_state").unwrap();
+    let desired_state = gate::State::from_str(lock_state_param);
+
+    if desired_state.is_ok() {
+        // TODO: TTL is passed in and server_config has a max TTL setting
+        // Note: a real TTL would be like 15-60 minutes
+        let lock_added: bool;
+        unsafe {
+            lock_added = GATE.as_mut().unwrap().hold_state(desired_state.unwrap(), std::time::Duration::from_secs(180));
+        }
+        if lock_added == false {
+            let json_response = "{\"error\": \"Could not move to desired_state. Most likely due to a lock to a different state.\"}".to_string();
+            return Some(easy_json_response(json_response));
+        }
+        return None;
+    } else {
+        return Some(invalid_state_response(lock_state_param));
+    }            
+}
+
+// POST /gate
+fn post_gate(req: Request<Body>) -> Response<Body> {
+    let mut operation_taken = false;
+    let params = get_params(&req);
+
+    if params.contains_key("desired_state") {
+        operation_taken = true;
+        let result = set_desired_state(&req);
+        if result.is_some() {
+            return result.unwrap();
+        }
+    }
+
+    if params.contains_key("lock_state") {
+        operation_taken = true;
+        let result = lock_state(&req);
+        if result.is_some() {
+            return result.unwrap();
+        }
+    }
+
+    if operation_taken == false {
+        let body = Body::from("{\"error\": \"no operation requested\"}");
+        let mut response = Response::new(body);
+        *response.status_mut() = StatusCode::BAD_REQUEST;
+        return response;
+    } else {
+        return get_gate();
+    }
+}
+
+// 404 handler
+pub fn page_not_found() -> Response<Body> {
+    let mut response = Response::default();
+    *response.status_mut() = StatusCode::NOT_FOUND;
+    return response;
+}
+
+pub async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            return Ok(get_root());
+        }
+        (&Method::GET, "/gate") => {
+           return Ok(get_gate());
+        },
+        (&Method::POST, "/gate") => {
+            return Ok(post_gate(req));
+        }
+        _ => {
+            return Ok(page_not_found());
+        }
+    }
+}
